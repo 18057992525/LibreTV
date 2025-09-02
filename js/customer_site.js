c7320bb1a81e58df6f45e82db355fb543d993f6a
Ruilin  Electronics <39164745+18057992525@users.noreply.github.com>
2025-09-02 18:11:48 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,12 +1,16 @@
 const CUSTOMER_SITES = {
     mozhua: {
         api: 'https://mozhuazy.com/api.php/provide/vod',
-        name: '魔爪资源',
-         api: 'https://ikunzyapi.com/api.php/provide/vod',
-        name: 'ikun',
-         api: 'https://bfzyapi.com/api.php/provide/vod',
-        name: '暴风资源',
-        }
+        name: '魔爪资源'
+    },
+    ikun: {
+        api: 'https://ikunzyapi.com/api.php/provide/vod',
+        name: 'ikun'
+    },
+    bfzy: {
+        api: 'https://bfzyapi.com/api.php/provide/vod',
+        name: '暴风资源'
+    }
     };
 // 调用全局方法合并
 if (window.extendAPISites) {
