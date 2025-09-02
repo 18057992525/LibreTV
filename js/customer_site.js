6af57500b7dcc00f52b911fc4e4853fee57b4f14
Ruilin  Electronics <39164745+18057992525@users.noreply.github.com>
2025-09-02 15:59:25 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,11 +1,8 @@
 const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
-    mozhua: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '魔爪',
-    }
+    : {
+        api: 'https://mozhuazy.com/api.php/provide/vod',
+        name: '魔爪资源',
+     }
 };
 
 // 调用全局方法合并
