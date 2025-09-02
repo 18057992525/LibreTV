9183ba4d90b3e5e39d122b3bdaffdd804f8c352c
Ruilin  Electronics <39164745+18057992525@users.noreply.github.com>
2025-09-02 17:55:15 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,6 +2,9 @@ const CUSTOMER_SITES = {
     mozhua: {
         api: 'https://mozhuazy.com/api.php/provide/vod',
         name: '魔爪资源',
+    ikun: {
+        api: 'https://ikunzyapi.com/api.php/provide/vod',
+        name: 'ikun',
      }
 };
 
